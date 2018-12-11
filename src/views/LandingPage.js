2b25a8c281605ad725c3baba2a7f3f6eb4baaa6a
Patrick Biffle <pbiffle@gmail.com>
2018-12-11 13:39:21 -0700

    destructured props in landingpage, WORKING

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -3,13 +3,12 @@ import { connect } from 'react-redux'
 import { emailChanged, passwordChanged } from '../actions'
 // import { link } from '@reach/router'
 
-const LandingPage = (props) => {
-  console.log(props)
+const LandingPage = ({ email, password, emailChanged }) => {
   return (
     <div>
       <form>
         <label>Username</label>
-        <input type='text' onChange={(e) => props.emailChanged(e.target.value)} value={props.email} />
+        <input type='text' onChange={(e) => emailChanged(e.target.value)} value={email} />
         <label>Password</label>
         <input type='text' />
       </form>
@@ -19,7 +18,6 @@ const LandingPage = (props) => {
 
 const mapStateToProps = state => {
   const { email, password } = state.auth
-
   return { email, password }
 }
 

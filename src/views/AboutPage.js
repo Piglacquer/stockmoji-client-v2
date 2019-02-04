feff08787b5761dc50e8d19c6f01f0eb54d20c8b
Patrick Biffle <pbiffle@gmail.com>
2019-02-04 11:46:53 -0700

    added initial copy and layout to about page

--- src/views/AboutPage.js
+++ src/views/AboutPage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+import '../styles/css/AboutPage.css'
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>ABOUT</h1>
+    <div id='about-page'>
+      <div className='about-block-emoji'>
+        <span className='about-emoji' role='img' aria-labelledby='cool-guy emoji'>😎</span>
+        <Typography className='about-text' component='h1'> Stockmoji is a application built for you, the market-inclined individual, who has trouble parsing through all of the market news to see whether the outlook of a particular stock is good, bad, or uncertain (yeah, it's uncertain a lot of the time).</Typography>
+      </div>
+      <div className='about-block-emoji'>
+        <Typography className='about-text'> Stockmoji is a application built for you, the market-inclined individual, who has trouble parsing through all of the market news to see whether the outlook of a particular stock is good, bad, or uncertain (yeah, it's uncertain a lot of the time).</Typography>
+        <span className='about-emoji' role='img' aria-labelledby='cool-guy emoji'>😎</span>
+      </div>
     </div>
   )
 }
